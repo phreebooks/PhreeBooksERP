005e7098f377774003dcfcfc814da4669a8a0d12
Rene Kervezee <rene.kervezee@gmail.com>
2014-04-03 11:23:45 +0200

    Fix for master stock copying updated inventory to 3.7.1

--- trunk/modules/inventory/classes/type/mb.php
+++ trunk/modules/inventory/classes/type/mb.php
@@ -61,9 +61,9 @@ class mb extends inventory {//Master Build (combination of Master Stock Item and
 			$result = $db->Execute("select * from " . TABLE_INVENTORY_MS_LIST . " where sku = '" . $this->old_sku . "'");
 			$data_array = array(
 				'sku'         => $this->sku,
-				'attr_0'      => $result->fields['ms_attr_0'],
+				'ms_attr_0'   => $result->fields['ms_attr_0'],
 				'attr_name_0' => $result->fields['attr_name_0'],
-				'attr_1'      => $result->fields['ms_attr_1'],
+				'ms_attr_1'   => $result->fields['ms_attr_1'],
 				'attr_name_1' => $result->fields['attr_name_1']);
 			db_perform(TABLE_INVENTORY_MS_LIST, $data_array, 'insert');
 			$result = $db->Execute("select * from " . TABLE_INVENTORY_MS_LIST . " where sku = '" . $this->old_sku . "'");
