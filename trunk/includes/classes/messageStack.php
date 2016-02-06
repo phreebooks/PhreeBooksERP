474bcb516229db0b5558e403e16918471852177d
Rene Kervezee <rene.kervezee@gmail.com>
2016-02-06 16:29:01 +0100

    update

--- trunk/includes/classes/messageStack.php
+++ trunk/includes/classes/messageStack.php
@@ -60,7 +60,7 @@ class messageStack {
 
     Static function debug_log ($txt){
     	global $admin;
-    	error_log("date: " . date('Y-m-d H:i:s') . " company:" .\core\classes\user::get_company(). " user: ".\core\classes\user::get('display_name'). ' ' . $txt . PHP_EOL, 3, DIR_FS_MY_FILES."development.log");
+    	error_log("date: " . date('Y-m-d H:i:s.u') . " company:" .\core\classes\user::get_company(). " user: ".\core\classes\user::get('display_name'). ' ' . $txt . PHP_EOL, 3, DIR_FS_MY_FILES."development.log");
     	if (substr($txt, 0, 1) == "\n") {
     		error_log("\nTime: " . (int)(1000 * (microtime(true) - PAGE_EXECUTION_START_TIME)) . " ms, " . $admin->DataBase->count_queries . " SQLs " . (int)($admin->DataBase->total_query_time * 1000)." ms => ".substr($txt, 1). PHP_EOL, 3, DIR_FS_MY_FILES."debug.log");
     	}else {
